bb08d17787fd9bf274ad6854f6c037767dc05337
Rafeek Alas <rafeekalas.official@gmail.com>
2023-08-25 22:48:31 +0530

    Stage 01 - Data Ingestion Completed

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ steps to implement this projects from scratch
 4. Update requirements.txt 
 5. update setup file  #2nd commit
 6.
+
+repeat the following steps for all the modules/stages
+1. Update logging info in src/projectname/_init_.py 
+2. Update config.yaml, params.yaml, schema.yaml
+3. Update config_entity.py
+4. update config.py #configuration manager
+4. update components #data_ingestion, data_validation, data_transformation, model_training, model_evaluation
+5. update pipeline stages #data_ingestion, data_validation, data_transformation, model_training, model_evaluation
+6. update main.py
---
+++ main.py
@@ -0,0 +1,18 @@
+from src.MLOpsProject import logger
+from src.MLOpsProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+#from src.MLOpsProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+#from src.MLOpsProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+#from src.MLOpsProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+#from src.MLOpsProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
